123e1e9958485ae79c7518914218363a6153df7b
ReySG <reinaldox99@gmail.com>
2020-12-31 12:57:19 -0300

    Primer Commit Prod

---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,29 @@
+//Comando para establecer conexion
+var socket = io();
+var label = $('#lblNuevoTicket')
+
+socket.on('connect', () => {
+    console.log('Conectado al servidor');
+});
+
+socket.on('disconnect', () => {
+    console.log('desconectado del servidor');
+});
+
+//on 'estadoActual'
+socket.on('estadoActual', function(resp) {
+
+    console.log(resp);
+    label.text(resp.actual);
+
+});
+
+$('button').on('click', function() {
+
+    socket.emit('siguienteTicket', null, function(siguienteTicket) {
+
+        label.text(siguienteTicket);
+
+    });
+
+});
